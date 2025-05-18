3a21459a7756577e441119e7908f3256d8b4cb17
Ryhnzzz <rayhanfahreza56@gmail.com>
2025-05-18 14:08:38 +0800

    feat(index) : add cors to allow cross origin request

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const app = express();
 
 const bookRoutes = require("./routes/bookRoutes");
 const memberRoutes = require("./routes/memberRoutes");
 const apiErrorHandler = require("./errors/apiErrorHandler");
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (isNaN(PORT)) {
-  console.error("PORT environment variable is invalid or missing.");
-  process.exit(1);
-}
-
 app.use(cors());
+
+// Middleware JSON
 app.use(express.json());
 
+// Routes
 app.use("/api/books", bookRoutes);
 app.use("/api/members", memberRoutes);
 
+// Error Handler
 app.use(apiErrorHandler);
 
 app.listen(PORT, () => {
